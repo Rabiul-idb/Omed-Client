ecfb82ac476a9acc19e9297aa49418d1a27a110c
rabiul-idb <rabiul.idb40@gmail.com>
2025-01-21 15:47:12 +0600

    delete a category done

--- src/Dashboard/ManageCategory.jsx
+++ src/Dashboard/ManageCategory.jsx
@@ -5,6 +5,7 @@ import { useQuery } from '@tanstack/react-query';
 import { FaEdit } from 'react-icons/fa';
 import { MdDeleteForever } from 'react-icons/md';
 import AddCategory from '../Modals/AddCategory';
+import Swal from 'sweetalert2';
 
 const ManageCategory = () => {
 
@@ -15,11 +16,37 @@ const ManageCategory = () => {
             return response.data;
         },
     });
-    
+
     if(isLoading){
         return <Loading></Loading>
     }
     
+    const handleDeleteCategory = (id) =>{
+        //console.log(id)
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then ((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/deleteCategory/${id}`)
+                .then(res => {
+                    if(res.data.deletedCount > 0){
+                        refetch();
+                          Swal.fire({
+                            title: "Deleted!",
+                            text: "Category has been deleted.",
+                            icon: "success"
+                        });
+                    }
+                })
+            }
+        });
+    }
 
     return (
         <div>
@@ -64,7 +91,7 @@ const ManageCategory = () => {
                         <th className='space-x-4'>
                         <button className="btn btn-ghost btn-xs">
                             <FaEdit className="text-xl text-info" /></button>
-                        <button className="btn btn-ghost btn-xs">
+                        <button onClick={()=> handleDeleteCategory(category._id)} className="btn btn-ghost btn-xs">
                             <MdDeleteForever className="text-2xl text-red-600" />
                         </button>
                         </th>
